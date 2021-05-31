e2501fd4d854c3db3b22010c6c0283af1d6ce188
Craskeyb <craskeybt@gmail.com>
2021-05-31 13:29:59 -0400

    Update gitignore for local PGNs, tested PGN reader

--- main.py
+++ main.py
@@ -3,4 +3,35 @@
 #Utilized python-chess library (listed in requirements.txt file)#
 
 import chess
+import chess.pgn
+import chess.svg
+
+print("Welcome to the Chess Recall Program!")
+print("\n-------------------------------------")
+filename = input("\nPlease enter the name of your PGN file (without the .pgn at the end): ")
+
+readfile = True
+
+filename = f"{filename}.pgn"
+
+pgn = open(filename)
+
+game = chess.pgn.read_game(pgn)
+
+print(game.headers["White"]+ ' vs. ' + game.headers["Black"])
+
+print(game)
+
+board = game.board()
+
+squares = chess.SquareSet(chess.BB_ALL)
+chess.svg.board(board,squares=squares, size=400)
+
+
+
+
+
+
+
+
 
