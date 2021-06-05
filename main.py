f0e7029baaf756e9f757746f03691b6cb6a557a4
Craskeyb <craskeybt@gmail.com>
2021-06-05 19:15:49 -0400

    finished pgnReader for now, handles errors well

--- main.py
+++ main.py
@@ -3,15 +3,18 @@
 #Utilized python-chess library (listed in requirements.txt file)#
 
 import chess
+import chess.pgn
 
 import chess.svg
 from pgnReader import gameRead
-import randPos
+#import randPos
 
 print("Welcome to the Chess Recall Program!")
 print("\n-------------------------------------")
 
 gameList = gameRead()
+
+
 
 
 
--- pgnReader.py
+++ pgnReader.py
@@ -2,43 +2,78 @@
 import chess
 import chess.pgn
 
+
 def gameRead():
     print("***You must enter a minimum of 3 games to choose from (2 games would be too easy to guess from!)")
     
-    #Create an empty list for loaded games
+    #Create an empty list for loaded games/filenames
     gameList = []
+    fileList = [] 
+    filename = ''
 
     #While loop based on counter
     while(len(gameList) < 3):
 
-        #make sure there is no file or the file is not already in the gamelist
-        while(filename == '' or filename not in gameList):
-        
         #grab files from user and check if there is an error
-            try:
-                filename = input("\nPlease enter the name of your PGN file (without the .pgn at the end): ")
-            except FileNotFoundError:
-                print("\nFile not found, try a different filename!")
-
-        #read in game
+        filename = input("\nPlease enter the name of your PGN file (without the .pgn at the end): ")
+        
+        if(filename in fileList):
+            print("\nGame has already been entered, please choose a different file!")
+            continue
         file = f"{filename}.pgn"
-        pgn = open(file)
+        try:
+            pgn = open(file)
+        except FileNotFoundError:
+            print("\nFile not found, Try another filename")
+            continue
         game = chess.pgn.read_game(pgn)
 
         #check to make sure it is over the required move count (20)
-        board = game.board()
         movect = 0
-        for move in game.maineline_moves():
-            board.push(move)
-            movect = movect+1
+        for move in game.mainline_moves():
+            movect = movect+.5
         
         if(movect <= 20):
-            print("\n Not enough moves in this game, miniatures (<=20 moves) are not allowed!")
-
-
-
-        gameList.append(chess.pgn.read_game(pgn))
+            print("\nNot enough moves in this game, miniatures (<=20 moves) are not allowed!")
+            continue
+        else:
+            print("\nSuccess! Game added to collection")
+            gameList.append(chess.pgn.read_game(pgn))
+            fileList.append(filename)
     
-    print
-    return gameList
     
+    while(1):
+        userAns = input("\nWould you like to enter additional games?(Y/N)")
+        if(userAns.upper() != 'N' and userAns.upper() != 'Y'):
+            print("\nPlease choose either yes(Y) or no(N)")
+            continue
+        elif(userAns.upper() == 'Y'):
+            #grab files from user and check if there is an error
+            filename = input("\nPlease enter the name of your PGN file (without the .pgn at the end): ")
+        
+            if(filename in fileList):
+                print("\nGame has already been entered, please choose a different file!")
+                continue
+            file = f"{filename}.pgn"
+            try:
+                pgn = open(file)
+            except FileNotFoundError:
+                print("\nFile not found, Try another filename")
+                continue
+            game = chess.pgn.read_game(pgn)
+
+        #check to make sure it is over the required move count (20)
+            movect = 0
+            for move in game.mainline_moves():
+                movect = movect+.5
+        
+            if(movect <= 20):
+                print("\nNot enough moves in this game, miniatures (<=20 moves) are not allowed!")
+                continue
+            else:
+                print("\nSuccess! Game added to collection")
+                gameList.append(chess.pgn.read_game(pgn))
+                fileList.append(filename)
+                continue
+        else:
+            return gameList
