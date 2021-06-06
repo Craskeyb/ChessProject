5084f27bfbbd3828fb0d6f2ed46928a0b7fc380f
Craskeyb <craskeybt@gmail.com>
2021-06-05 22:30:32 -0400

    Called randPos for testing in main

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 #Project created by Brett Craskey#
 
-#Utilized python-chess library (listed in requirements.txt file)#
-
+#Utilizes python-chess library (listed in requirements.txt file)#
 import chess
 import chess.pgn
-
-import chess.svg
 from pgnReader import gameRead
-#import randPos
+from randPos import posPic
 
 print("Welcome to the Chess Recall Program!")
 print("\n-------------------------------------")
 
 gameList = gameRead()
+
+posPic(gameList)
 
 
 
